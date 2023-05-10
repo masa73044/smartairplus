c0f869052eaec596be2a85d234415d3c8cefb0af
masa73044 <masa7304@gmail.com>
2023-05-10 15:16:25 +0700

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
 const App = () => {
-  return <div id="dashboard">Test</div>;
+  return (
+    <div id="dashboard">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
