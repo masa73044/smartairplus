d6b822e2f8db64d339d66c98d85a27e8e73fe247
masa73044 <masa7304@gmail.com>
2023-05-12 15:29:06 +0700

    fully completed dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Calendar from "./pages/Calendar/Calendar";
+import DataGrid from "./pages/DataGrid/DataGrid";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Layout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="calendar" element={<Calendar />} />
+            <Route path="users" element={<DataGrid />} />
           </Route>
         </Routes>
       </BrowserRouter>
