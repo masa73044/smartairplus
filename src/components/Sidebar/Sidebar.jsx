6844b913ae83eb7615c1144c6d113da11f14c53f
masa73044 <masa7304@gmail.com>
2023-05-15 16:25:34 +0700

    sidebar icon

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,8 @@
 import css from "./Sidebar.module.css";
 import { MdSpaceDashboard } from "react-icons/md";
 import { AiFillCalendar, AiOutlineTable } from "react-icons/ai";
+import { GiEarthAmerica } from "react-icons/gi";
+
 import { FaTasks } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
@@ -15,7 +17,9 @@ const Sidebar = () => {
         <NavLink to="calendar" className={css.item} title="Calendar">
           <AiFillCalendar size={30} />
         </NavLink>
-        <NavLink to="board" className={css.item} title="Trello Board"></NavLink>
+        <NavLink to="calendar" className={css.item} title="Global Map">
+          <GiEarthAmerica size={30} />
+        </NavLink>
         <NavLink to="users" className={css.item} title="Users">
           <AiOutlineTable size={30} />
         </NavLink>
